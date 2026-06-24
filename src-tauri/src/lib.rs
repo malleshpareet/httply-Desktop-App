12e8f8abe241d5e46fd4fdc50aeb76a296fe5883
Mallesh Pareet <malleshpareet360@gmail.com>
2026-06-24 16:10:29 +0530

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,34 @@
+use tauri::Manager;
+#[cfg(target_os = "windows")]
+use window_vibrancy::apply_mica;
+
+#[cfg(target_os = "macos")]
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+  tauri::Builder::default()
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.handle().plugin(
+          tauri_plugin_log::Builder::default()
+            .level(log::LevelFilter::Info)
+            .build(),
+        )?;
+      }
+      
+      let window = app.get_webview_window("main").unwrap();
+
+      #[cfg(target_os = "windows")]
+      apply_mica(&window, Some(true))
+        .expect("Unsupported platform! 'apply_mica' is only supported on Windows 11");
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
